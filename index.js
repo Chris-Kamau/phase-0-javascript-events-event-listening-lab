21e68bb0c8debbd3ffe37379c0474a07c2a7d058
Chris <chris.kamau@student.moringaschool.com>
2023-03-23 14:31:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
 }
+
+function addingEventListener() {
+    const input = document.querySelector('input');
+    input.addEventListener('click', function() {
+      console.log('True');
+    });
+  }
+  
